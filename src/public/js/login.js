02794563a80e2cb49598e1cb476bf1724b160c5b
Francisco Zuidwijk <electrocisko@gmail.com>
2022-11-02 19:37:44 -0300

    Commit

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -12,21 +12,25 @@ const form = document.getElementById("loginForm");
  }
 
 form.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  let data = new FormData(form);
-  let obj = {};
-  data.forEach((value, key) => (obj[key] = value));
-  fetch("/api/sessions/login", {
-    method: "POST",
-    body: JSON.stringify(obj),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((result) => result.json())
-    .then(json => succes(json))
-    .catch((error) => {
-      console.log(`Error en peticion login.js fetch: ${error}`);
-    });
-  //form.reset();
+  try {
+    evt.preventDefault();
+    let data = new FormData(form);
+    let obj = {};
+    data.forEach((value, key) => (obj[key] = value));
+    fetch("/api/sessions/login", {
+      method: "POST",
+      body: JSON.stringify(obj),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((result) => result.json())
+      .then(json => succes(json))
+      .catch((error) => {
+        console.log(`Error en peticion login.js fetch: ${error}`);
+      });
+    //form.reset();
+  } catch (error) {
+      console.log('Error en login', error)
+  }
 });
